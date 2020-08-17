bdebbcc76003f05783e14c5a2a8b209b35232256
Sparhawk <sparhawktheartist@gmail.com>
2020-08-17 15:25:15 -0700

    everything else

--- js/scripts.js
+++ js/scripts.js
@@ -1,72 +1,51 @@
 $('.grid').masonry({
-    itemSelector: '.gridItem',
+    itemSelector: '.grid.Item',
     columnWidth: '.gridSizer',
     percentPosition: true,
-    gutter: 0
-    // stagger: 30,
+    gutter: 0,
+    stagger: 30
     // transitionDuraction: '.5s'
-})
+});
 
-// function mason() {
-//     $('.grid').masonry({
-//     itemSelector: '.gridItem',
-//     columnWidth: '.gridSizer',
-//     percentPosition: true,
-//     gutter: 0
-//     // stagger: 30,
-//     // transitionDuraction: '.5s'
-// })}
-// function filter(rndm){
-//     $('.gridItem').addClass('invis');
-//     $('.rndm').removeClass('invis');
-//     mason();
-// }
-
-// // $(document).ready(mason())
-
-// $('.button1').click(function(){
-//     $('.gridItem').removeClass('invis');
-//     mason();
-// })
-// $('.button2').click(function(){
-//     $('.gridItem').addClass('invis');
-//     $('.bird').removeClass('invis');
-//     mason();
-// })
-
-// $('.button3').click(function(){
-//     $('.gridItem').addClass('invis');
-//     $('.bee').removeClass('invis');
-//     mason();
-// })
-
-// $('.button4').click(function(){
-//     $('.gridItem').addClass('invis');
-//     $('.nature').removeClass('invis');
-//     mason();
-// })
-// $('.button5').click(function(){
-//     $('.gridItem').addClass('invis');
-//     $('.landscape').removeClass('invis');
-//     mason();
-// })
-// $('.button6').click(function(){
-//     $('.gridItem').addClass('invis');
-//     $('.macro').removeClass('invis');
-//     mason();
-// })
-// $('.button7').click(function(){
-//     $('.gridItem').addClass('invis');
-//     $('.architecture').removeClass('invis');
-//     mason();
-// })
-// $('.button8').click(function(){
-//     $('.gridItem').addClass('invis');
-//     $('.forest').removeClass('invis');
-//     mason();
-// })
-
-
-
+$('.button8').click(function(){
+    $('.gridItem').hide();
+    $('.forest').show();
+    $('.filter').removeClass('filtered')
+    $(this).addClass('filtered')
+});
+$('.button7').click(function(){
+    $('.gridItem').hide();
+    $('.architecture').show();
+    $('.filter').removeClass('filtered')
+    $(this).addClass('filtered')
+});
+$('.button6').click(function(){
+    $('.gridItem').hide();
+    $('.macro').show();
+    $('.filter').removeClass('filtered')
+    $(this).addClass('filtered')
+});
+$('.button5').click(function(){
+    $('.gridItem').hide();
+    $('.landscape').show();
+    $('.filter').removeClass('filtered')
+    $(this).addClass('filtered')
+});
+$('.button4').click(function(){
+    $('.gridItem').hide();
+    $('.nature').show();
+    $('.filter').removeClass('filtered')
+    $(this).addClass('filtered')
+});
+$('.button2').click(function(){
+    $('.gridItem').hide();
+    $('.bird').show();
+    $('.filter').removeClass('filtered')
+    $(this).addClass('filtered')
+});
+$('.button1').click(function(){
+    $('.gridItem').show();
+    $('.filter').removeClass('filtered')
+});
 
     
