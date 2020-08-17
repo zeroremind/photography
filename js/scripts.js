135d93fb7a2a352579d7a58d780d5682707a8a96
Sparhawk <sparhawktheartist@gmail.com>
2020-08-17 13:59:07 -0700

    basic structure

---
+++ js/scripts.js
@@ -0,0 +1,72 @@
+$('.grid').masonry({
+    itemSelector: '.gridItem',
+    columnWidth: '.gridSizer',
+    percentPosition: true,
+    gutter: 0
+    // stagger: 30,
+    // transitionDuraction: '.5s'
+})
+
+// function mason() {
+//     $('.grid').masonry({
+//     itemSelector: '.gridItem',
+//     columnWidth: '.gridSizer',
+//     percentPosition: true,
+//     gutter: 0
+//     // stagger: 30,
+//     // transitionDuraction: '.5s'
+// })}
+// function filter(rndm){
+//     $('.gridItem').addClass('invis');
+//     $('.rndm').removeClass('invis');
+//     mason();
+// }
+
+// // $(document).ready(mason())
+
+// $('.button1').click(function(){
+//     $('.gridItem').removeClass('invis');
+//     mason();
+// })
+// $('.button2').click(function(){
+//     $('.gridItem').addClass('invis');
+//     $('.bird').removeClass('invis');
+//     mason();
+// })
+
+// $('.button3').click(function(){
+//     $('.gridItem').addClass('invis');
+//     $('.bee').removeClass('invis');
+//     mason();
+// })
+
+// $('.button4').click(function(){
+//     $('.gridItem').addClass('invis');
+//     $('.nature').removeClass('invis');
+//     mason();
+// })
+// $('.button5').click(function(){
+//     $('.gridItem').addClass('invis');
+//     $('.landscape').removeClass('invis');
+//     mason();
+// })
+// $('.button6').click(function(){
+//     $('.gridItem').addClass('invis');
+//     $('.macro').removeClass('invis');
+//     mason();
+// })
+// $('.button7').click(function(){
+//     $('.gridItem').addClass('invis');
+//     $('.architecture').removeClass('invis');
+//     mason();
+// })
+// $('.button8').click(function(){
+//     $('.gridItem').addClass('invis');
+//     $('.forest').removeClass('invis');
+//     mason();
+// })
+
+
+
+
+    
