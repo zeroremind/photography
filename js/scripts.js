3f5edfcd595093e82503068304e166f20d49de32
Sparhawk <sparhawktheartist@gmail.com>
2020-08-18 12:52:17 -0700

    mobile styling

--- js/scripts.js
+++ js/scripts.js
@@ -50,4 +50,68 @@ $('.button8').click(function(){
     $('.filter').removeClass('filtered')
     $(this).addClass('filtered')
 });
-    
+
+
+
+
+// if ($rndmFilter == selected){
+//     $selected[content].append ($rndmFilter[value])
+// }
+mediaQuery = window.matchMedia("(max-width: 950px)");
+
+
+
+
+    if (mediaQuery['matches'] == true) {
+        $(function(){
+            $('.menu').hide();
+            $('.mobileMenu').show();
+            $('.filter').hide();
+            $('.selected').show();
+       });
+        $('.selected').append('Conceptual' + ' &#9776;')            
+        $(function(){
+            $('.selected').click(function(){
+                $('.filter').show();
+                $('.selected').hide();
+            });
+            $('.button1').click(function(){
+                $('.selected').empty();
+                $('.selected').prepend('All'+'&#9776;')
+                $('.filter').hide();
+                $('.selected').show();
+            });
+            
+            $('.button4').click(function(){
+                $('.selected').empty();
+                $('.selected').prepend('Nature' +' &#9776;')
+                $('.filter').hide();
+                $('.selected').show();
+            });
+            $('.button5').click(function(){
+                $('.selected').empty();
+                $('.selected').prepend('Landscapes' +' &#9776;')
+                $('.filter').hide();
+                $('.selected').show();
+            });
+            
+            $('.button6').click(function(){
+                $('.selected').empty();
+                $('.selected').prepend('Macro' +' &#9776;')
+                $('.filter').hide();
+                $('.selected').show();
+            });
+            $('.button7').click(function(){
+                $('.selected').empty();
+                $('.selected').prepend('Architecture' +' &#9776;')
+                $('.filter').hide();
+                $('.selected').show();
+            });
+            $('.button8').click(function(){
+                $('.selected').empty();
+                $('.selected').prepend('Conceptual' +' &#9776;')
+                $('.filter').hide();
+                $('.selected').show();
+            });
+        });
+    };
